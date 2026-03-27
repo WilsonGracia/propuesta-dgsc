beed22f83a1a2f4e94f751d487dd55f194021734
JosthynVillalobos <yosthyn2000@gmail.com>
2026-03-27 15:32:34 -0600

    Update main.ts
    Prueba

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+//Hola wacho probando que tengo permisos
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
